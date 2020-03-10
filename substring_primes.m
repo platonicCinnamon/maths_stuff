94e55eebead938bd655ac73310c818e305f9a740
platonicCinnamon <39978886+platonicCinnamon@users.noreply.github.com>
2020-03-09 21:22:45 -0300

    Optimised stuff and better typing

--- substring_primes.m
+++ substring_primes.m
@@ -1,22 +1,18 @@
-r = Table[Prime[n],{n,1,78498}] (* 78498 is pi(1,000,000). the actual number i should have used is 5761455, pi(100,000,000) but got confused *)
+n = 78498; (* 78498 is pi(1,000,000). The actual number I should have used is 5761455, pi(100,000,000), but got confused *)
 
-primeQ1 = (#//PrimeQ)(* ||#==1 *)&  (* uncommenting the OR will give the sequence of numbers in which every substring is noncomposite *)
+primeQ1 = (#//PrimeQ)(* ||#==1 *)&  (* Uncommenting the OR will give the sequence of numbers in which every substring is noncomposite *)
 
-subs = ReplaceList[ (* gives a list with every non empty substring of # *)
+subs = ReplaceList[ (* Gives a list with every non empty substring of # *)
   Characters @ #, 
   {___, sub__, ___} :> StringJoin[sub]
 ]&
 
-subprimeQ = If[
-  (#//ToString//subs//ToExpression//DeleteDuplicates//Cases[Except[_?primeQ1]]//Length) > 0,
-  False, (*This will save a step later on 16*)
-  True
-]&
+subprimeQ = !TrueQ[(#//ToString//subs//ToExpression//DeleteDuplicates//Cases[Except[_?primeQ1]]//Length) > 0]&
 
 Do[
   If[
-    r[[n]]//subprimeQ,
-    r[[n]]//Print
+    i//Prime//subprimeQ,
+    i//Prime//Print
   ],
-  {n,1,78498}
+  {i,1,n}
 ]
